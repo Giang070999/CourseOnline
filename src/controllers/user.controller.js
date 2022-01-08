3c334429b65492b054f6f51f60fd5da4cf71b881
Hoang Nam <efert269@gmail.com>
2022-01-08 21:32:23 +0700

    fix view grade

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -305,7 +305,8 @@ const getMyGrade = async (req, res, next) => {
         }
         // check khoá học kết thúc chưa?
         const classs = await ClassModel.findOne({ code: classCode })
-        if (!classs.complete) return res.status(401).json({ message: "Chưa thể xem điểm." })
+        // console.log(classs.complete);
+        // if (!classs.complete) return res.status(401).json({ message: "Chưa thể xem điểm." })
 
         // lấy bảng điểm
         const result = await GradeModel.find(query).select("-__v -_id")
